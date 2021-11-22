7f8b94d6095ba6e4652be43a76d963b1e52925bc
qwibbler <88380694+qwibbler@users.noreply.github.com>
2021-11-22 15:48:26 -0600

    Fix Local storage and break remove button

--- script.js
+++ script.js
@@ -13,18 +13,35 @@ let collection = [
   },
 ];
 
+function getValue() {
+  if (localStorage.getItem('collection')) {
+    console.log(collection);
+    collection = JSON.parse(localStorage.getItem('collection'));
+    console.log(collection);
+  }
+}
+
+getValue()
+window.onload = getValue;
+
 function catchValue() {
   const saveCollection = JSON.stringify(collection);
   localStorage.setItem('collection', saveCollection);
 }
-
-function getValue() {
-  collection = JSON.parse(localStorage.getItem('collection'))
-}
-
-window.onload = getValue;
+catchValue()
 
 const booksWrapper = document.querySelector('.books');
+
+function removeBook(book) {
+  collection.splice(book, 1);
+  booksWrapper.removeChild(booksWrapper.childNodes[book + 1]);
+  catchValue();
+}
+
+const allRemove = document.querySelectorAll('.remove-btn');
+for (let i = 0; i < allRemove.length; i += 1) {
+  allRemove[i]
+}
 
 function printBook(book) {
   const eachBook = document.createElement('div');
@@ -33,7 +50,7 @@ function printBook(book) {
   const eachAuthor = document.createElement('li');
   const eachRemove = document.createElement('li');
   const removeBtn = document.createElement('button');
-  
+
   eachBook.appendChild(eachList);
   eachList.appendChild(eachTitle);
   eachList.appendChild(eachAuthor);
@@ -48,12 +65,16 @@ function printBook(book) {
   booksWrapper.appendChild(eachBook);
 }
 
+
 for (let i = 0; i < collection.length; i += 1) {
   printBook(collection[i]);
 }
 
+console.log('after print', collection);
+
 function addBook(title, author) {
   collection.push({ title: title, author: author });
+  printBook(collection[collection.length - 1]);
   catchValue();
 }
 
@@ -63,19 +84,4 @@ const addBtn = document.querySelector('#addBtn');
 
 addBtn.addEventListener('click', () => {
   addBook(title.value, author.value);
-  printBook(collection[collection.length-1]);
 })
-
-function removeBook(book) {
-  collection = collection.splice(book, 1);
-  booksWrapper.removeChild(booksWrapper.childNodes[book+1]);
-  catchValue();
-}
-
-
-const allRemove = document.querySelectorAll('.remove-btn');
-for (let i = 0; i < allRemove.length; i += 1){
-  allRemove[i].addEventListener('click', () => {
-    removeBook(i)
-  })
-}
