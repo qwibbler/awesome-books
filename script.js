46b7db2b563c8be2880cbaa0f8afe23d40ee260f
joseheco <jferhc@gmail.com>
2021-11-22 15:02:28 -0500

    add js array

---
+++ script.js
@@ -0,0 +1,15 @@
+const collection = [
+{ title: book1,
+  author: Zaman,
+},
+{ title: book2,
+  author: Jose,
+},
+{ title: book3,
+  author: Herrera,
+},
+];
+
+function addBook(title, author) {
+  collection.push({title: title, author: author});
+}
