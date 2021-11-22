eef54616aac5cc4bf18b009489b71c751d624788
qwibbler <88380694+qwibbler@users.noreply.github.com>
2021-11-22 17:33:56 -0600

    Clean up and fix linter errors.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Get Collection
+// Default
 let collection = [
   {
     title: 'book1',
@@ -17,42 +19,49 @@ let collection = [
 ];
 let idCode = 3;
 
-function getValue() {
-  if (localStorage.getItem('collection')) {
-    console.log('notNull', localStorage.getItem('collection'));
-    collection = JSON.parse(localStorage.getItem('collection'));
-    idCode = JSON.parse(localStorage.getItem('idCode'));
-    console.log('notNull', localStorage.getItem('collection'));
-  }
-  catchValue();
-}
-
-getValue()
-window.onload = getValue;
-
+// Save it to localStorage
 function catchValue() {
   const saveCollection = JSON.stringify(collection);
   const saveIdCode = JSON.stringify(idCode);
   localStorage.setItem('collection', saveCollection);
   localStorage.setItem('idCode', saveIdCode);
 }
-// catchValue()
 
+// Get it from localStorage (if it exists)
+function getValue() {
+  if (localStorage.getItem('collection')) {
+    collection = JSON.parse(localStorage.getItem('collection'));
+    idCode = JSON.parse(localStorage.getItem('idCode'));
+    catchValue();
+  }
+}
+// Run function
+getValue();
+window.onload = getValue;
+
+// Print Books + Add/Remove
+// Books Section
 const booksWrapper = document.querySelector('.books');
 
+// Remove function by id
 function removeBook(idCode) {
-  collection.filter(function (book) {
-    if (book.idCode === idCode){
-      index = collection.indexOf(book);
-      collection.splice(index.idCode, 1);
+  // Remove from collection
+  collection.filter((book) => {
+    if (book.idCode === idCode) {
+      const index = collection.indexOf(book);
+      collection.splice(index, 1);
     }
+    return book;
   });
-  const toRemove = document.getElementById(`${idCode}`)
+  // Remove from print
+  const toRemove = document.getElementById(`${idCode}`);
   booksWrapper.removeChild(toRemove);
   catchValue();
 }
 
+// Print a book
 function printBook(book) {
+  // Create elements
   const eachBook = document.createElement('div');
   const eachList = document.createElement('ul');
   const eachTitle = document.createElement('li');
@@ -60,42 +69,44 @@ function printBook(book) {
   const eachRemove = document.createElement('li');
   const removeBtn = document.createElement('button');
 
+  // Append elements
   eachBook.appendChild(eachList);
-  eachBook.id = `${book.idCode}`
+  eachBook.id = `${book.idCode}`;
   eachList.appendChild(eachTitle);
   eachList.appendChild(eachAuthor);
   eachList.appendChild(eachRemove);
   eachRemove.appendChild(removeBtn);
+  booksWrapper.appendChild(eachBook);
+
+  // Add values
+  eachTitle.textContent = book.title;
+  eachAuthor.textContent = book.author;
   removeBtn.textContent = 'Remove';
   removeBtn.className = 'remove-btn';
+
+  // Add event listener
   removeBtn.addEventListener('click', () => removeBook(book.idCode));
-
-  eachTitle.innerHTML = book.title;
-  eachAuthor.innerHTML = book.author;
-
-  booksWrapper.appendChild(eachBook);
 }
 
+// Print every book already in collection
 for (let i = 0; i < collection.length; i += 1) {
   printBook(collection[i]);
 }
 
+// Add book function
 function addBook(title, author) {
-  collection.push({ title: title, author: author, idCode: idCode});
+  collection.push({ title, author, idCode });
   idCode += 1;
   printBook(collection[collection.length - 1]);
   catchValue();
 }
 
+// Get inputs
 const title = document.querySelector('#title');
 const author = document.querySelector('#author');
 const addBtn = document.querySelector('#addBtn');
 
+// On click, add book to collection
 addBtn.addEventListener('click', () => {
   addBook(title.value, author.value);
-})
-
-// const allRemove = document.querySelectorAll('.remove-btn');
-// for (let i = 0; i < allRemove.length; i += 1) {
-//   allRemove[i].addEventListener('click', () => removeBook(i))
-// }
+});
