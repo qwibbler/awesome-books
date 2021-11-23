eba38c589fc0e7809590d87caf9100c715fd6f10
joseheco <jferhc@gmail.com>
2021-11-23 11:59:23 -0500

    adding class to add and removebook

---
+++ class.js
@@ -0,0 +1,34 @@
+class book {
+  constructor(title, author, idCode) {
+    this.title = title;
+    this.author = author;
+    this.idCode = idCode;
+  }
+}
+
+class collection {
+
+
+  addbook(title, author, idCode) {
+    const newbook = new book(title, author, idCode);
+    collection.push(newbook);
+    idCode += 1;
+  }
+
+  removebook (idCode) {
+    collection.filter((book) => {
+      if (book.idCode === idCode) {
+        const index = collection.indexOf(book);
+        collection.splice(index, 1);
+      }
+      return book;
+    });
+    // Remove from print
+    const toRemove = document.getElementById(`${idCode}`);
+    booksWrapper.removeChild(toRemove);
+    catchValue();
+  }
+  
+}
+
+
